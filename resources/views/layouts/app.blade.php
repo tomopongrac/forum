7ba3220e45a99fbcf14640c57c6629285d151f62
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-05-04 17:48:16 +0200

    A user can filter threads by any username

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,8 +35,17 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
-                    <li>
-                        <a class="nav-link" href="{{ route('threads.index') }}">All Threads</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Browse
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('threads.index') }}">All Threads</a>
+                            @if (auth()->check())
+                                <a class="dropdown-item" href="{{ route('threads.index').'?by='.auth()->user()->name }}">My Threads</a>
+                            @endif
+                        </div>
                     </li>
                     @if (auth()->check())
                         <li>
@@ -50,7 +59,8 @@
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             @foreach($channels as $channel)
-                                <a class="dropdown-item" href="{{ route('threads.channel', ['channel' => $channel->slug ]) }}">{{ $channel->name }}</a>
+                                <a class="dropdown-item"
+                                   href="{{ route('threads.channel', ['channel' => $channel->slug ]) }}">{{ $channel->name }}</a>
                             @endforeach
                         </div>
                     </li>
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Channel;
 use App\Reply;
 use App\Thread;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -56,5 +57,17 @@ class ReadThreadsTest extends TestCase
         $this->get(route('threads.channel', ['channel' => $channel->slug]))
             ->assertSee($threadInChannel->title)
             ->assertDontSee($threadNotInChannel->title);
+    }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_any_username()
+    {
+        $user = create(User::class, ['name' => 'JohnDoe']);
+        $threadByJohn = create(Thread::class, ['user_id' => $user->id]);
+        $threadNotByJohn = create(Thread::class);
+
+        $this->get(route('threads.index').'?by='.$user->name)
+            ->assertSee($threadByJohn->title)
+            ->assertDontSee($threadNotByJohn->title);
     }
 }
