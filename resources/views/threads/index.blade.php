167218abc2e12851f07ca564d06d780740fe3016
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-05-06 20:25:24 +0200

    Modifying layout for showing all threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,33 +4,26 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Forum Threads</div>
+                <h1>Forum Threads</h1>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+
+                @foreach($threads as $thread)
+                    <div class="card mb-3">
+                        <div class="card-header">
+                            <div class="level">
+                                <h4 class="flex">
+                                    <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">
+                                        {{ $thread->title }}
+                                    </a>
+                                </h4>
+                                <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
                             </div>
-                        @endif
+                        </div>
 
-                        @foreach($threads as $thread)
-                            <article>
-                                <div class="level">
-                                    <h4 class="flex">
-                                        <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">
-                                            {{ $thread->title }}
-                                        </a>
-                                    </h4>
-                                    <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
-                                </div>
-                                <div class="body">{{ $thread->body }}</div>
-                            </article>
-
-                            <hr/>
-                        @endforeach
+                        <div class="card-body">{{ $thread->body }}</div>
                     </div>
-                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
