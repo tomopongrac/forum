4dde2524348c0a12ea50e504635866b1039a10b7
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-05-09 19:47:37 +0200

    It records activity when thread is created

--- app/Thread.php
+++ app/Thread.php
@@ -15,6 +15,29 @@ class Thread extends Model
         static::addGlobalScope('replyCount', function ($builder) {
             $builder->withCount('replies');
         });
+
+        static::created(function ($thread) {
+            $thread->recordActivity('created');
+        });
+    }
+
+    protected function recordActivity($event)
+    {
+        if (auth()->guest()) {
+            return;
+        }
+
+        Activity::create([
+            'user_id' => auth()->id(),
+            'type' => $this->getActivityType($event),
+            'subject_id' => $this->id,
+            'subject_type' => get_class($this),
+        ]);
+    }
+
+    protected function getActivityType($event): string
+    {
+        return $event.'_'.strtolower((new \ReflectionClass($this))->getShortName());
     }
 
     public function replies()
---
+++ tests/Unit/ActivityTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Thread;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_records_activity_when_a_thread_is_created()
+    {
+        $user = create(User::class);
+
+        $this->actingAs($user);
+        $thread = create(Thread::class);
+
+
+        $this->assertDatabaseHas('activities', [
+            'user_id' => auth()->id(),
+            'type' => 'created_thread',
+            'subject_id' => $thread->id,
+            'subject_type' => 'App\Thread',
+        ]);
+    }
+}
