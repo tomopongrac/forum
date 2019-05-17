8cdeea22cdc1530241ab79421f40ecee72c5666e
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-05-17 21:19:37 +0200

    If there is no activity for user show appropriate text

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,11 +8,13 @@
                 <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h1>
         </div>
-        @foreach($activities as $date => $dateActivities)
+        @forelse($activities as $date => $dateActivities)
             <h2>{{ $date }}</h2>
             @foreach($dateActivities as $activity)
                 @includeIf("profiles.activities.{$activity->type}")
             @endforeach
-        @endforeach
+        @empty
+            <p>There is no activity for this user yet.</p>
+        @endforelse
     </div>
 @endsection
