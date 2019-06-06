885cb5d5b533fb889b6dada4d76a861125ed1779
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-06-06 21:29:43 +0200

    Creating blade partial for list of threads and adding creator of thread

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,23 @@
+@foreach($threads as $thread)
+    <div class="card mb-3">
+        <div class="card-header">
+            <div class="level">
+                <div class="flex">
+                    <h4>
+                        <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">
+                            @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                                <strong>{{ $thread->title }}</strong>
+                            @else
+                                {{ $thread->title }}
+                            @endif
+                        </a>
+                    </h4>
+                    <h5>Posted By: <a href="{{ route('profiles.show', ['name' => $thread->creator->name]) }}">{{ $thread->creator->name }}</a> </h5>
+                </div>
+                <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
+            </div>
+        </div>
+
+        <div class="card-body">{{ $thread->body }}</div>
+    </div>
+@endforeach
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,29 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h1>Forum Threads</h1>
-
-
-                @foreach($threads as $thread)
-                    <div class="card mb-3">
-                        <div class="card-header">
-                            <div class="level">
-                                <h4 class="flex">
-                                    <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">
-                                        @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                            <strong>{{ $thread->title }}</strong>
-                                        @else
-                                            {{ $thread->title }}
-                                        @endif
-                                    </a>
-                                </h4>
-                                <a href="{{ route('threads.show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
-                            </div>
-                        </div>
-
-                        <div class="card-body">{{ $thread->body }}</div>
-                    </div>
-                @endforeach
-
+                @include('threads._list')
             </div>
         </div>
     </div>
